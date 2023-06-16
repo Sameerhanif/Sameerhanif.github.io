fe643b6811520ceecdd1a1a11e3992ef58786c4d
Sameerhanif <112457325+Sameerhanif@users.noreply.github.com>
2023-06-16 23:52:47 +0200

    fast

---
+++ JS/Main.js
@@ -0,0 +1,51 @@
+console.log('hello world')
+
+function homepage()
+{
+
+    window.location = '/HTML/homepage.html'
+
+}
+
+
+function Hobbies ()
+{
+   window.location = "/HTML/hobbies.html"
+}
+
+
+function werk()
+{
+
+window.location = "/HTML/werk.html"
+
+}
+
+function Skills(){
+    window.location = "/HTML/Skills.html"
+}
+
+function extra(){
+    window.location="/HTML/extra.html"
+}
+
+function facebook(){
+    window.location.href = "https://www.facebook.com/";
+}
+
+function instragram(){
+    window.location.href = "https://www.instagram.com/sam_yeeeeet/";
+}
+
+function twitter() {
+    window.location.href = "https://twitter.com/YEE36602501";
+
+}
+
+function BKE() {
+    window.location="/games/BKE/BKE.html"
+}
+
+function HL() {
+    window.location="/games/HL/HL.html"
+}
